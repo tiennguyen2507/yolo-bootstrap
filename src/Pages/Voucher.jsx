7dce2e59545568c52b2278f8f50a13b23d63b744
tien nguyen <nguyenledinhtien2507>
2021-08-07 10:08:31 +0700

    go

--- src/Pages/Voucher.jsx
+++ src/Pages/Voucher.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import { v4 } from 'uuid'
+import { Container } from 'react-bootstrap'
+import banner from '../assets/images/panner02.png'
 
 const Voucher = () => {
-    document.title='Mã giảm giá'
+    document.title = 'Mã giảm giá'
 
     return (
-        <div>
-             {v4()}   
-        </div>
+        <div className='voucher'>
+            <Container>
+                <div className='voucher__banner'>
+                    <img src={banner} alt="" />
+                </div>
+            </Container>
+            <div className="voucher__code">
+                <div className="voucher__code__title">
+                    <h1>Nhận mã giảm giá 20%</h1>
+                </div>
+                <div className="voucher__code__code01">
+                    <div className="voucher__code__code01__09">
+                        <div>
+                            <h2 className="voucher__code__code01__09__01"> Mã giảm giá</h2>
+
+                        </div>
+                        <div>
+                            <h2 className="voucher__code__code01__09__01">0708QUABANMOI25K</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div >
     )
 }
 
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -6,13 +6,15 @@ import Home from '../Pages/Home'
 import Products from '../Pages/Products'
 import Login from '../Pages/Login'
 import Voucher from '../Pages/Voucher'
+import Product from '../Pages/Product'
+
 
 const Router = () => {
     return (
         <Switch>
             <Route path ='/' exact component={Home} />
             <Route path ='/product/:slug' component={Products} />
-            <Route path ='/product/'    component={Products} />
+            <Route path ='/product/' exact   component={Product} />
             <Route path ='/cart' component={Card} />
             <Route path ='/about' component={About} />
             <Route path ='/login' component={Login} />
